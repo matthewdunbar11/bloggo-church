468dd314a42997e177cfecf6a448517a91c1133a
Matthew Dunbar <matthewdunbar11@gmail.com>
2015-05-19 21:52:51 -0400

    Adds latest sermons widget

--- bloggo-church.php
+++ bloggo-church.php
@@ -4,19 +4,21 @@
 	GitHub Plugin URI: https://github.com/matthewdunbar11/bloggo-church
 	GitHub Branch: master
 	Description: Provides content types and fields for Bloggo Media websites
-	Version: 0.0.2 
+	Version: 1.0.3
 */
 
 include( 'taxonomies/speaker.php' );
 include( 'taxonomies/sermon_series.php' );
 include( 'content-types/sermons.php' );	
 include( 'acf/acf.php' );
+include( 'widgets/latest_sermons.php' );
 
 class Bloggo_Church {
 	public function __construct() {	
 		add_action( 'init', 'register_sermon_post_type', 0 );
 		add_action( 'init', 'register_speaker_taxonomy', 0 );
 		add_action( 'init', 'register_sermon_series_taxonomy', 0 );
+		add_action( 'widgets_init', 'register_latest_sermons_widget' );
 		add_action( 'init', array( $this, 'init' ) );
 		add_filter( 'the_content', array( $this, 'the_content' ) );
 		//add_filter( 'template_include', array($this, 'template_include' ) );
@@ -46,6 +48,7 @@ class Bloggo_Church {
 				. $the_content;
 			
 			$file = get_field('file');
+			$media_shortcode = '';
 			if(substr($file['mime_type'], 0, 5) == 'audio') {
 				$media_shortcode = '[audio src="' . $file['url'] . '"]';
 			}
