473c1c9aaa54a54111d15c8f4d6d4166978d1274
Matthew Dunbar <matthewdunbar11@gmail.com>
2015-05-19 22:03:49 -0400

    Changes version number

--- bloggo-church.php
+++ bloggo-church.php
@@ -4,7 +4,7 @@
 	GitHub Plugin URI: https://github.com/matthewdunbar11/bloggo-church
 	GitHub Branch: master
 	Description: Provides content types and fields for Bloggo Media websites
-	Version: 1.0.2 
+	Version: 1.0.3
 */
 
 include( 'taxonomies/speaker.php' );
